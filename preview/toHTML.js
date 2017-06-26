aaf68169a34a83fd43adba4ba9b3dce762f5e7c8
JDMCreator <jdmcreator@hotmail.fr>
2017-06-26 16:18:56 -0400

    LaTeXTableEditor Preview

---
+++ preview/toHTML.js
@@ -0,0 +1,10 @@
+(function(){
+	var getHTMLFromCell = function(cell){
+		return "";
+	}
+	table.createInterpreter("md", function(table){
+
+		var matrix = table.getMatrixOfCells();
+		return "";
+	})
+})();
