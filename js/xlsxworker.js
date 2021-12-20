32288c3eee026a991abe2593306129c6d1019de0
JDMCreator <xavier.dionne@hotmail.com>
2021-12-20 16:18:37 -0500

    Version 2.3
    See changelog

--- js/xlsxworker.js
+++ js/xlsxworker.js
@@ -5,10 +5,24 @@ postMessage({t:"ready"});
 onmessage = function (evt) {
   var workbook;
   try {
-    workbook = XLSX.read(evt.data.d, {type: evt.data.b});
+    workbook = XLSX.read(evt.data.d, {type: evt.data.b,cellDates:true});
     // Now we convert it to HTML and send it back
     var results = [];
     workbook.SheetNames.forEach(function(sheetName) {
+	if(evt.data.m || evt.data.c){
+	var sheet = workbook.Sheets[sheetName];
+		for(var i in sheet){
+			if(sheet.hasOwnProperty(i) && i[0] != "!"){
+				if(sheet[i].t == "n"){
+					var n = sheet[i].w;
+					if(evt.data.c){
+						n = n.replace(/,/g,".").replace(/\.(?=[^\.]*$)/g,",").replace(/\./g,"");
+					}
+					sheet[i].w = n;
+				}
+			}
+		}
+	}
 	var htmlstr = XLSX.write(workbook, {sheet:sheetName, type:'string', bookType:'html'});
 	results.push({name:sheetName, html : htmlstr}); 
    });
